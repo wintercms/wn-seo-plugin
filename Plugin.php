50dae0b06809763349b7cd8b79622f51137c9e08
Vadia Bezdushny <element.ua@gmail.com>
2023-04-10 19:13:17 +0300

    + support of external model metadata by metadata_from public property

--- Plugin.php
+++ Plugin.php
@@ -8,6 +8,66 @@ use Winter\SEO\Classes\Meta;
 use Winter\SEO\Models\Settings;
 use Yaml;
 
+/**
+ * SEO Plugin Information File
+ * @TODO:
+ * - Support for robots.txt, humans.txt, and security.txt
+ *     - humans.txt prepopulate from project composer.json?
+ *     - Twig parsing in .txt file definitions
+ *     - When adding support for twig parsing also add heavy caching by default for parsed results
+ * - Support for Model Behavior that creates meta data in separate DB
+ *     - check for extended with in the form event listener
+ *     - Support templated strings for meta data managed by plugin (i.e. {{ record.name }} - {{ record.category }})
+ * - Support for Winter.Translate
+ * - Automatic extension of Winter.Blog
+ * - Recommended plugins: Winter.Sitemap, Winter.Pages, Winter.Blog, Winter.Redirect
+ * - Documentation
+ * - All settings should be backed / default to config values for file based configuration
+ * - Title prefix / suffix; includeTitle default true in component
+ * - Comments / help text for all provided fields ideally with references on how they should be used
+ * - Rename "Meta" tab to "SEO" tab
+ * - Character limit on description field
+ * - Full support for common robots settings via meta tags and site wide defaults
+ * - Default addition of generator meta tag in config file
+ * - Support for "Social Media" quick connects (FB / Twitter IDs / accounts) (maybe just via global tags)
+ * - Support for more sources of og:image (i.e. manual URL, fileupload, mediafinder, etc)
+ * - Favicon, webmanifest, browserconfig, apple touch icons, etc
+ * - Previews of FB / Twitter / Google
+ * - Support for structured data
+ * - Winter.Redirect
+ *     - Model behavior to setup automatic redirects when URL attribute changes
+ * - Support for Winter.Builder pages
+ * - Support for twig parsing of meta tag values?
+ * - og:locale / other relevant locale flags, perhaps provide from Winter.Translate plugin? Or just from this plugin
+ * - Contextual settings for adding records to the sitemap rather than having to add everything in the sitemap main section? Perhaps implement in Winter.Sitemap
+ *
+ * @Related:
+ * - https://github.com/wintercms/wn-redirect-plugin
+ * - https://github.com/wintercms/wn-sitemap-plugin
+ * - https://github.com/wintercms/wn-pages-plugin
+ * - https://github.com/wintercms/wn-blog-plugin
+ * - https://github.com/wintercms/wn-translate-plugin
+ *
+ * @See also:
+ * - https://github.com/bennothommo/wn-meta-plugin
+ * - https://github.com/mjauvin/wn-mlsitemap-plugin
+ * - https://github.com/josephcrowell/wn-sitemappretty-plugin
+ * - https://github.com/studiobosco/wn-seo-extension
+ * - https://github.com/WebVPF/wn-robots-plugin
+ * - https://octobercms.com/plugin/renatio-seomanager
+ * - https://octobercms.com/plugin/lovata-mightyseo
+ * - https://octobercms.com/plugin/initbiz-seostorm
+ * - https://octobercms.com/plugin/zen-robots
+ * - https://octobercms.com/plugin/utopigs-seo
+ * - https://octobercms.com/plugin/mohsin-txt
+ * - https://octobercms.com/plugin/linkonoid-schemamarkup
+ * - https://octobercms.com/plugin/vdlp-schemaorg
+ * - https://octobercms.com/plugin/egalhtn-localbusiness
+ * - https://octobercms.com/plugin/dynamedia-posts
+ * - https://octobercms.com/plugin/eugene3993-seo
+ * - https://octobercms.com/plugin/magiczne-seotweaker
+ * - https://octobercms.com/plugin/eugene3993-seolight
+ */
 class Plugin extends PluginBase
 {
     /**
@@ -29,6 +89,7 @@ class Plugin extends PluginBase
     public function boot(): void
     {
         $this->extendPagesForms();
+        $this->extendExternalPlugins();
         $this->populateGlobalTags();
     }
 
@@ -38,12 +99,8 @@ class Plugin extends PluginBase
     protected function extendPagesForms()
     {
         $controllerModels = [
-            \Cms\Controllers\Index::class => [
-                \Cms\Classes\Page::class,
-            ],
-            \Winter\Pages\Controllers\Index::class => [
-                \Winter\Pages\Classes\Page::class,
-            ],
+            \Cms\Controllers\Index::class => [ \Cms\Classes\Page::class ],
+            \Winter\Pages\Controllers\Index::class => [ \Winter\Pages\Classes\Page::class ],
         ];
 
         Event::listen('backend.form.extendFieldsBefore', function (\Backend\Widgets\Form $widget) use ($controllerModels) {
@@ -119,6 +176,57 @@ class Plugin extends PluginBase
             }
         });
 
+    }    
+    
+    /**
+     * Extends external plugins with support for SEO form fields
+     */
+    protected function extendExternalPlugins(): void
+    {
+        // TODO
+        // Maybe move metadata field to single pivot table like [id, model, model_id, metadata]
+        $controllerModels = [
+            \Winter\Blog\Controllers\Posts::class => [
+                \Winter\Blog\Models\Post::class,
+            ],
+            \Winter\Blog\Controllers\Categories::class => [
+                \Winter\Blog\Models\Category::class,
+            ],
+        ];
+
+        Event::listen('backend.form.extendFieldsBefore', function (\Backend\Widgets\Form $widget) use ($controllerModels) {
+            if($widget->isNested) return;
+            $controller = $widget->getController();
+            $controllerClass = get_class($controller);
+            $model = $widget->model;
+            $modelClass = get_class($model);
+            $shouldExtend = property_exists($model, 'metadata_from') || 
+                            (isset($controllerModels[$controllerClass]) && 
+                            in_array($modelClass, $controllerModels[$controllerClass]));
+
+            if(!$shouldExtend) return;
+
+            // Create link to proper widget tabs container
+            if($widget->secondaryTabs) {
+              $tabs = &$widget->secondaryTabs;
+            } else {
+              $tabs = &$widget->tabs;
+            }
+
+            // Model's metadata column can be set explicitly `public $metadata_from = 'column_name';`
+            $metadataField = strlen(trim($model->metadata_from)) ? $model->metadata_from : 'metadata';
+
+            $form = Yaml::parseFile(plugins_path('winter/seo/models/meta/fields.yaml'));
+            $tab = 'winter.seo::lang.models.meta.label';
+            $fields = [];
+            foreach ($form['fields'] as $name => $config) {
+              $config['tab'] = $tab;
+              $fields["{$metadataField}[seo][{$name}]"] = $config;
+            }
+            $tabs['paneCssClass'][$tab] = 'padded-pane';
+            $tabs['icons'][$tab] = 'icon-magnifying-glass';
+            $tabs['fields'] = array_merge($tabs['fields'], $fields);
+        });
     }
 
     /**
